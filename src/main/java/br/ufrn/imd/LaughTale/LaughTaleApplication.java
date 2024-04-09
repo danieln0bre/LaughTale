48db4321a0c87aa2b1d7af408fb6f9d0c5e430cb
daniel <danielnobre09@gmail.com>
2024-04-09 08:40:53 -0300

    first ommit

---
+++ src/main/java/br/ufrn/imd/LaughTale/LaughTaleApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.LaughTale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaughTaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaughTaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufrn/imd/LaughTale/LaughTaleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.LaughTale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaughTaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
